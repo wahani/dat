09a2fb41f0520c54ac673ddd4cdbcaacccd91b41
Sebastian <wahani@gmail.com>
2016-07-15 20:33:13 +0200

    properly reexport pipe

--- R/pipeExport.R
+++ R/pipeExport.R
@@ -1,13 +1,2 @@
-#' @title Piping operator
-#' @name %>%
-#'
-#' @description This is the 'pipe operator' from the package 'magrittr':
-#'  \link[magrittr]{\%>\%}.
-#'
-#' @param lhs The value to be piped
-#' @param rhs A function or expression
-#'
-#' @export %>%
-#' @rdname pipe
-#' @usage lhs \%>\% rhs
-NULL
+#' @export
+magrittr::"%>%"
