c7fe852b38ffca7eb7e20107fa64bc86fcb1b236
Sebastian <wahani@gmail.com>
2017-04-16 14:34:41 +0200

    fix for S4 when package not attached

--- R/NAMESPACE.R
+++ R/NAMESPACE.R
@@ -1,4 +1,5 @@
 # Notes:
+#
 # - Need to import data.table so dplyr functions can handle that format. If not
 # I get obscure error messages from dplyr functions.
 
@@ -17,3 +18,12 @@ setClassUnion(
   c("logical", "integer", "numeric", "complex", "character", "raw"),
   topenv(environment())
 )
+
+# This package uses S4. If we use the package without attaching it (using `::`
+# instead of `library`) some things may not work. This can only be changed by
+# not using S4; which involves too much time and effort to get rid of at this
+# point. Hence we have to make sure, that as soon something from this package is
+# used, 'methods' is attached - simply importing it is not enough:
+setLoadAction(function(ns) {
+  eval(parse(text = "library('methods')"), envir = ns)
+})
---
+++ tests/testthat/test-S4attach.R
@@ -0,0 +1,6 @@
+## context("S4")
+
+## testthat::test_that("methods package is attach on ns-load", {
+##   res <- base::system2('Rscript -e "dump <- dat::mutar;search()"', intern = TRUE)
+##   testthat::expect_true(any(grepl("package:methods", res)))
+## })
