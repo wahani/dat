71e5b713f7598530b806f5b72f5450a1a1b47af5
Sebastian <wahani@gmail.com>
2018-01-05 11:12:06 +0100

    fix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,5 @@
 library("dat")
 
 if (requireNamespace("testthat", quietly = TRUE)) {
-  files <- list.files("testthat", full.names = TRUE)
-  for (fname in files) source(files)
+  testthat::test_check("dat")
 }
