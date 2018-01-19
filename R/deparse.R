d05b58a33f41c89f737c0a8f51799cd4a24c5d2c
Sebastian <wahani@gmail.com>
2018-01-19 19:58:56 +0100

    CRAN relase 0.4.0

--- R/deparse.R
+++ R/deparse.R
@@ -3,6 +3,6 @@ deparse <- function(x) {
   # https://bugs.r-project.org/bugzilla/show_bug.cgi?id=17378
   # which led to an incorrect behavior when x is an S4 object containing
   # a formula. This function will override the default behaviour of base to
-  # conform with the idea of of how deparse should work within 'dat'
+  # conform with the idea of how 'deparse' should work within 'dat'.
   base::deparse(asS3(x))
 }
